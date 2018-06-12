e76b360fbd2fc413c8c1d3107999761502d1a0d0
Luiz Abud <39606135+abudluiz@users.noreply.github.com>
2018-06-12 16:23:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php 
+           include ("head.php");
+        ?>
+    </head>
+    <body>
+    <div class="container">
+        <header>
+             <?php 
+                 include ("header.php");
+             ?>
+        </header>
+		<!-- banner destaque -->
+		    <section class="banners row">
+				<div class="banner col-12">
+					<img src="img/destaqueHome.jpg" alt="banner">
+				</div>
+			</section>
+            <br>
+            <br>
+			<!-- produtos -->
+			<div class="cont-prod">
+			<section class="vip-products row">
+				<article class="product col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 01">
+					<h2>Produto 01</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+				<article class="product col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 02">
+					<h2>Produto 02</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+				<article class="product nomargin col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 03">
+					<h2>Produto 03</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+				<article class="product col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 04">
+					<h2>Produto 04</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+				<article class="product col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 05">
+					<h2>Produto 05</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+				<article class="product nomargin col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 06">
+					<h2>Produto 06</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+                </article>
+                <article class="product nomargin col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 07">
+					<h2>Produto 07</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+                    <a href="#">ver mais</a>
+                </article>
+                <article class="product nomargin col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 08">
+					<h2>Produto 08</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+                    <a href="#">ver mais</a>
+                </article>
+                <article class="product nomargin col-12 col-sm-6 col-md-4">
+					<img src="#" alt="pdto 09">
+					<h2>Produto 09</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+					<a href="#">ver mais</a>
+				</article>
+			</section>
+        </div>
+    <br>
+    <br>
+    </body>
+    <footer>
+            <?php 
+                 include ("footer.php");
+             ?>
+    </footer>
+</html>
---
+++ perguntas.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php 
+           include ("head.php");
+        ?>
+    </head>
+
+    <body>
+        <Header>
+        <?php 
+           include ("header.php");
+        ?> 
+        </Header>
+        <br>
+        <br>
+        <!-- Perguntas Frequentes -->
+        <section>
+            <h1>
+                Perguntas Frequentes
+            </h1>
+             <ul>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, consectetur?</a></li>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit?</a></li>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum, consectetur adipisicing elit?</a></li>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, elit?</a></li>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor  elit?</a></li>
+                <li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, adipisicing elit?</a></li>
+                <li class="lista_perguntas"><a href="#">Lorem ipsum, consectetur adipisicing elit?</a></li>
+				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, elit?</a></li>
+			    <li class="lista_perguntas"><a href="#">Lorem ipsum dolor  elit?</a></li>
+			    <li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, adipisicing elit?</a></li>
+             </ul>
+        </section>     
+        <br>
+        <br>
+        <!-- Fale Conosco -->
+        <h2>
+            Não encontrou uma resposta? Podemos te ajudar.
+        </h2>
+        <?php 
+           include ("contato.php");
+        ?> 
+        <br>
+        <br>
+        <a href="index.html">Voltar Home</a> 
+        <br>
+        <br>
+        <footer>
+        <?php 
+           include ("footer.php");
+        ?> 
+        </footer>  
+    </body>
+</html>
