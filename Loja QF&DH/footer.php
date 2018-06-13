468faf38b6bbc97b35b872ec8b315fba209945b2
dpicinat <danielpicinato@hotmail.com>
2018-06-13 19:36:40 -0300

    Inserido modificacoes para alinhar banner e consolidado campos ao header e inserido campo pesquisa

--- Loja QF&DH/footer.php
+++ Loja QF&DH/footer.php
@@ -1,10 +1,10 @@
 <footer class="main-footer">
 			<ul>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="#">como comprar?</a></li>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="#">sobre nós</a></li>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="perguntas.php">perguntas frequentes</a></li>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="#">social</a></li>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="#">nossas lojas</a></li>
-				<li class="lista_footer col-12 col-sm-6 col-md-6"><a href="faleconosco.php">fale conosco</a></li>
+				<li class="lista_footer col-10 col-md-2"><a href="#">como comprar?</a></li>
+				<li class="lista_footer col-10 col-md-2"><a href="#">sobre nós</a></li>
+				<li class="lista_footer col-10 col-md-2"><a href="perguntas.php">perguntas frequentes</a></li>
+				<li class="lista_footer col-10 col-md-2"><a href="#">social</a></li>
+				<li class="lista_footer col-10 col-md-2"><a href="#">nossas lojas</a></li>
+				<!-- <li class="lista_footer col-12 col-sm-2 col-md-2 col-lg-2"><a href="faleconosco.php">fale conosco</a></li> -->
 			</ul>
 		</footer>
--- Loja QF&DH/header.php
+++ Loja QF&DH/header.php
@@ -1,21 +1,24 @@
 		<!-- cabeçalho / menu -->
 <header class="main-header row">
-			<div class="col-12 col-md-12">
-				<img src="img/logoQFDH.png" alt="logotipo" class="logo">
-            </div>
-            <div>
-                <a href="cadastro.php">Novo Cadastro</a>
-                <a href="login.php">Login</a>
+			<div class="row col-12 col-md-12">
+				<img src="../img/logo2.png" alt="logotipo" class="logo">
+								<form class="search" method="post" action="pagina_de_resultados.html">
+										<input class="fieldSearch" type="text" name="q" value="Pesquisar...">
+										<input class="buttonSearch" type="submit" value="Pesquisar">
+								</form>
+                <a href="cadastro.php" class="search">Novo Cadastro</a>
+                <a href="login.php" class="search">Login</a>
+								<a href="faleconosco.php" class="search">Contato</a>
             </div>
 			<div class="menustructure col-12 col-md-12">
 				<nav class="main-nav">
 					<ul class="navegacao row">
-						<li class="menu col-12 col-md-2"><a href="index.php">home</a></li>
-						<li class="menu col-12 col-md-2"><a href="#">lançamentos</a></li>
-						<li class="menu col-12 col-md-2"><a href="#">produtos</a></li>
-						<li class="menu col-12 col-md-2"><a href="#">promoções</a></li>
-						<li class="menu col-12 col-md-2"><a href="#">sobre nós</a></li>
-						<li class="menu col-12 col-md-2"><a href="faleconosco.php">contato</a></li>
+						<li class="menu col-10 col-md-2"><a href="index.php">home</a></li>
+						<li class="menu col-10 col-md-2"><a href="#">lançamentos</a></li>
+						<li class="menu col-10 col-md-2"><a href="#">produtos</a></li>
+						<li class="menu col-10 col-md-2"><a href="#">promoções</a></li>
+						<li class="menu col-10 col-md-2"><a href="#">sobre nós</a></li>
+						<!-- <li class="menu col-12 col-md-2"><a href="faleconosco.php">contato</a></li> -->
 					</ul>
 				</nav>
 			</div>
