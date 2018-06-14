c6bedf387c10308116a20e3b843eaf1f18224016
dpicinat <danielpicinato@hotmail.com>
2018-06-14 17:32:46 -0300

    adicionado carrossel no banner

--- Loja QF&DH/index.php
+++ Loja QF&DH/index.php
@@ -13,10 +13,30 @@
              ?>
         </header>
 		<!-- banner destaque -->
-		    <section class="banners row">
-				<div class="banner">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="../img/destaqueHome.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="../img/destaqueHome2.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="../img/destaqueHome3.jpg" alt="Third slide">
+      </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+      </div>
+				<!-- <div class="banner">
 					<img class="col 12" src="../img/destaqueHome.jpg" alt="banner">
-				</div>
+				</div> -->
 			</section>
             <br>
             <br>
@@ -81,10 +101,12 @@
         </div>
     <br>
     <br>
-    </body>
-    <footer>
             <?php
                  include ("footer.php");
              ?>
-    </footer>
+             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </body>
+
 </html>
