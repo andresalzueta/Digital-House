6a825deea4bb44c71a1d9715d6bc5bb8c5c09f38
dpicinat <danielpicinato@hotmail.com>
2018-09-09 13:22:49 -0300

    melhoria página de perguntas com card-header

--- Loja_QFDH/perguntas.php
+++ Loja_QFDH/perguntas.php
@@ -5,6 +5,8 @@
         <?php
            include ("head.php");
         ?>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/accordion.css">
     </head>
 
     <body>
@@ -27,9 +29,55 @@
           </div>
         </div>
 
+        <div class="perguntas_accordion">
+          <div id="accordion" style="text-align: center">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    Pergunta 1
+                  </button>
+                </h5>
+              </div>
 
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Pergunta 2
+                  </button>
+                </h5>
+              </div>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingThree">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Pergunta 3
+                  </button>
+                </h5>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-<div class="container">
+<!-- <div class="container">
   <div class="col-lg-12">
     <div class="userBox center-block">
         <section>
@@ -39,41 +87,21 @@
     				<li class="lista_perguntas"><a href="#">Lorem ipsum, consectetur adipisicing elit?</a></li>
     				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, elit?</a></li>
     				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor  elit?</a></li>
-            <li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, adipisicing elit?</a></li>
-            <li class="lista_perguntas"><a href="#">Lorem ipsum, consectetur adipisicing elit?</a></li>
-    				<li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, elit?</a></li>
-    			   <li class="lista_perguntas"><a href="#">Lorem ipsum dolor  elit?</a></li>
-    			   <li class="lista_perguntas"><a href="#">Lorem ipsum dolor sit amet, adipisicing elit?</a></li>
+
           </ul>
         </section>
     </div> <!-- <div class="loginBox center-block">-->
-  </div> <!-- </div colunas>-->
-</div> <!-- </div container>-->
+  <!-- </div> <!-- </div colunas>-->
+<!-- </div> <!-- </div container>-->
         <br>
         <br>
-        <!-- Fale Conosco -->
-        <div class="container">
-          <div class="row">
-            <div class="c-widget__header col-12">
-                <h2 class="text-center" >
-                  <p class="font-weight-light">Não encontrou uma resposta? Podemos te ajudar.</p>
-                </h2>
-            </div>
-          </div>
-        </div>
 
-
-
-
-        <?php
-           include ("contato.php");
-        ?>
-        <br>
-        <br>
         <div class="container">
           <div class="row">
             <div class="c-widget__header col-12">
                 <div class="text-center" >
+                  <a href="contato.php">Entrar em Contato</a>
+                  <br><br>
                   <a  href="index.html">Voltar Home</a>
               </div>
             </div>
@@ -86,5 +114,8 @@
            include ("footer.php");
         ?>
         </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     </body>
 </html>
