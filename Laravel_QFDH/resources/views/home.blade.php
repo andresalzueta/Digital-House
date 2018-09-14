0a372cd7664752c17524bbf7e379c4fd737fd0bb
dpicinat <danielpicinato@hotmail.com>
2018-09-13 22:09:27 -0300

    atualizado front end com as atualizações back do Andres

--- Laravel_QFDH/resources/views/home.blade.php
+++ Laravel_QFDH/resources/views/home.blade.php
@@ -303,71 +303,9 @@
             <div class="col-12">
                 <div class="text-center">
                 <hr class="Separador">
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>        
+            </div>
         </div>
     </div>
-
-    <div class="cont-prod row">
-        <section class="vip-products row">
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="img/Produtos/Ela/Jaqueta.jpg" class="sizeImg" alt="pdto 01" style="width:300px;height:300px;">
-					<!-- <h2>Produto 01</h2> -->
-					<h2>Jaquetas</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="img/Produtos/Ela/Óculos.jpg" class="sizeImg" alt="pdto 01" style="width:300px;height:300px;">
-                    
-                    <h2>Óculos</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="img/Produtos/Ela/Blazer.jpg" class="sizeImg" alt="pdto 01" style="width:300px;height:300px;">
-                    <h2>Lorem ipsum amet</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="images/img-pdto-1.jpg" alt="pdto 01">
-                    <h2>Lorem ipsum amet</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="images/img-pdto-2.jpg" alt="pdto 02">
-                    <h2>Lorem ipsum amet</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-            <article class="product col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="images/img-pdto-3.jpg" alt="pdto 03">
-                    <h2>Lorem ipsum amet</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
-                    <a href="#">ver mais</a>
-            </article>
-
-        </section>
-    </div>
-
-
-
-</div>
 
 
 
